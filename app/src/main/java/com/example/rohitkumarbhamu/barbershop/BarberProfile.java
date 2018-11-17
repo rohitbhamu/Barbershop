23aff51420f2d30af1be1fc1d8544d5432fd0bf2
rohitbhamu <rohitbhamu6@gmail.com>
2018-11-17 13:31:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohitkumarbhamu/barbershop/BarberProfile.java
@@ -0,0 +1,13 @@
+package com.example.rohitkumarbhamu.barbershop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarberProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barber_profile);
+    }
+}
